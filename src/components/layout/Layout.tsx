ed605eaff1e26a35bcd157f38917e7fb34ad7882
Ahmed Maher <89836955+ahmedmaherr@users.noreply.github.com>
2022-07-16 18:56:59 +0200

    feat: starting navbar and footer

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,16 @@
 import * as React from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <main className='bg-gray-900'>{children}</main>;
+  return (
+    <div className='flex min-h-screen flex-col bg-gray-900'>
+      <nav className='sticky top-0 flex items-center justify-between bg-black bg-opacity-10 py-4 px-6 backdrop-blur-md backdrop-saturate-150'>
+        <h3 className='text-white'>Logo</h3>
+        <ul>Menu</ul>
+      </nav>
+      <main className='flex-1'>{children}</main>
+      <footer>
+        <h1>Footer</h1>
+      </footer>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,22 +2,16 @@ import * as React from 'react';
 
 import Button from '@/components/buttons/Button';
 import Layout from '@/components/layout/Layout';
-import Seo from '@/components/Seo';
 
 export default function HomePage() {
   return (
     <Layout>
-      {/* <Seo templateTitle='Home' /> */}
-      <Seo />
-
-      <main className='flex h-screen w-screen items-center justify-center gap-4'>
+      <div className='flex h-full items-center justify-center gap-4'>
         <Button>Primary Button</Button>
         <Button variant='ghost'>Ghost Button</Button>
         <Button variant='light'>Light Button</Button>
         <Button variant='outline'>Outline Button</Button>
-      </main>
+      </div>
     </Layout>
   );
 }
-
-// flex-shrink-0 bg-purple-600 text-white text-base font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200
