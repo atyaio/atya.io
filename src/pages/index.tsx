94003eb1d3f4593d908032cb486eba2e8c6e2779
Ahmed Maher <89836955+ahmedmaherr@users.noreply.github.com>
2022-07-17 18:19:32 +0200

    chore: added title to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
+import Seo from '@/components/Seo';
 
 export default function HomePage() {
   return (
     <Layout>
+      <Seo templateTitle='Homepage' />
       <div className='flex h-full flex-wrap items-center justify-center gap-4'>
         <h1 className='text-white'>Homepage</h1>
         {/* <Button>Primary Button</Button>
