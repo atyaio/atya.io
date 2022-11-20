c7b77150591886b1696503fa41eefb99d33480bf
Ahmed Maher <89836955+ahmedmaherr@users.noreply.github.com>
2022-11-20 02:33:48 +0200

    multi-lang setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,40 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { IntlProvider } from "react-intl";
 
+import ar from "../lang/ar.json";
+import en from "../lang/en.json";
 import "../styles/globals.css";
 
+type SupportedLanguages = "ar" | "en";
+
+const messages = {
+  ar,
+  en,
+};
+
+function getDirection(locale: SupportedLanguages) {
+  if (locale === "ar") {
+    return "rtl";
+  }
+
+  return "ltr";
+}
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const { locale = "en" } = useRouter() as { locale: SupportedLanguages };
+
+  useEffect(() => {
+    const direction = getDirection(locale);
+    document.documentElement.dir = direction;
+  }, [locale]);
+
+  return (
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <Component {...pageProps} />
+    </IntlProvider>
+  );
 };
 
 export default MyApp;
